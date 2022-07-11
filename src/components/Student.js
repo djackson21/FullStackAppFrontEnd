3e68fd41207993f3a7f67351d8379933ca29b983
Derron Jackson <Derrnjackson01gmail.com>
2022-07-10 19:52:27 -0500

    Added button and const classes to view input on screen

--- src/components/Student.js
+++ src/components/Student.js
@@ -1,13 +1,16 @@
 import * as React from 'react';
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
-import { Container, Paper, paperClasses } from '@mui/material';
+import { Button, Container, Paper, paperClasses } from '@mui/material';
 
 export default function Student() {
   const paperClasses={padding:'50px 20px', width:600, margin:"20px auto"}
+  const[name,setName]=React.useState('')
+  const[address,setAddress]=React.useState('')
   return (
     <Container>
       <Paper elevation={3} style={paperClasses}>
+        <h1 style={{color:"blue"}}><u>Add Student</u></h1>
     <Box
       component="form"
       sx={{
@@ -16,9 +19,19 @@ export default function Student() {
       noValidate
       autoComplete="off"
     >
-      <TextField id="outlined-basic" label="Student Name" variant="outlined" fullWidth/>
-      <TextField id="outlined-basic" label="Student Address" variant="outlined" fullWidth/>
+      <TextField id="outlined-basic" label="Student Name" variant="outlined" fullWidth
+      value={name}
+      onChange={(e)=>setName(e.target.value)}
+      />
+      <TextField id="outlined-basic" label="Student Address" variant="outlined" fullWidth
+      value={address}
+      onChange={(e)=>setAddress(e.target.value)}
+      />
+    <Button variant='contained' color='secondary'>Submit</Button>
+
     </Box>
+    {name}
+    {address}
     </Paper>
     </Container>
   );
